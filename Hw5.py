0269e76944018be0301cec5c2c3db49054563ab8
mixologyy <108787185+mixologyy@users.noreply.github.com>
2022-08-09 13:54:58 +0700

    Add files via upload

---
+++ Hw5.py
@@ -0,0 +1,19 @@
+import random
+z=[]
+a=(input("Number: "))
+z.append(a)
+b=(input("Number: "))
+z.append(b)
+c=(input("Number: "))
+z.append(c)
+d=(input("Number: "))
+z.append(d)
+
+x=[]
+for i in range(24):
+    random.shuffle(z)
+    e=int(z[0]+z[1]+z[2]+z[3])
+    x.append(e)
+print("Max Number is",'{:,}'.format(max(x)))
+
+
